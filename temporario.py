0e46e3b639232936ab897248fd36a275c69b1763
docenteprpg <docente.pr.pg@gmail.com>
2025-02-26 20:01:00 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -3,4 +3,7 @@ st.header("Cabeçalho")
 st.toggle("Toggle")
 st.text_area("Enter text")
 st.text_input("")
+st.selectbox(
+  'Qual a sua cor favorita?',
+  ('Azul', 'Vermelho', 'Verde'))
 st.button("Botão Salvar")
